bb6189ac1a1d71d96190736164ab85c35aa0a627
derekbt96 <derekbt@yahoo.com>
2018-11-21 00:56:41 -0500

    Update to camera trigger

--- camera_control/telemGrab.py
+++ camera_control/telemGrab.py
@@ -7,15 +7,15 @@ while(True):
 	if msg is None:
 		#print 'found nothing'
 		with open('/home/pi/camera_control/telemLast.txt','w') as locFile:
-			locFile.write('0.0 0.0 0.0 0.0')
+			locFile.write('0.0-0.0-0.0-0.0')
 	else:
 		telemetry = [msg.lat/(1e7),msg.lon/(1e7),msg.alt*0.00328084,msg.hdg/100.0]
 
 		with open('/home/pi/camera_control/telemLast.txt','w') as locFile:
 			locFile.write(str(telemetry[0]))
-			locFile.write(' ')
+			locFile.write('-')
 			locFile.write(str(telemetry[1]))
-			locFile.write(' ')
+			locFile.write('-')
 			locFile.write(str(telemetry[2]))
-			locFile.write(' ')
+			locFile.write('-')
 			locFile.write(str(telemetry[3]))
